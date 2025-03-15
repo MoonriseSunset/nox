cd9d89583de58729b1c480b0c0f5763f7f284ee9
Moonrise (Desktop) <aliuesch@gmail.com>
2025-03-14 22:04:31 -0700

    applying fixes to other sites

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://moonrisesunset.dev',
+  site: 'https://moonrisesunset.github.io',
   base: '/nox',
   integrations: [	
     starlight({
