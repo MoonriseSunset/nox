5977dfb5091fcbfb5d9559dbce760a9991e92e6c
Moonrise (Desktop) <69921861+MoonriseSunset@users.noreply.github.com>
2025-03-22 21:27:04 -0700

    [3/22/2025] Spring Cleaning Migration

---
+++ astro.config.mjs
@@ -0,0 +1,38 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+import starlight from '@astrojs/starlight';
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://moonrisesunset.github.io',
+  base: '/nox',
+  integrations: [	
+    starlight({
+      title: "Nox",
+			customCss:[
+				'./src/styles/starlight.css',
+
+				'@fontsource/barlow',
+				'@fontsource/fira-mono',
+				'@fontsource-variable/dm-sans'
+			],
+			social: {
+				github: 'https://github.com/MoonriseSunset',
+			},
+			favicon: './assets/starlightfavicon.png',
+      		logo: {src:'./src/assets/Logo.png',},
+			sidebar: [
+				{
+					label: 'Start Here',
+					items:[
+						{label: 'Landing Page',link: '/'},
+						{label: 'Changelog',link: '/changelog/'},
+					
+					],
+					collapsed: false,
+				},
+			],
+		}),
+  ],
+});
